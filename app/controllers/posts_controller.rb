c10c8f653eeecb7ec0bb2b58a3cf2ff4ffbe43f9
khushboo <khushbooagrwal998@gmail.com>
2019-07-25 17:31:50 +0530

    Initial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@posts=Post.all
+	end
+
+	def new
+		@post=Post.new
+	end 
+
+	def create
+		@post=Post.new(post_params)
+		
+		if @post.save
+			redirect_to posts_path, :notice => "Your post was saved"
+		else
+			render "new"
+		end	
+	end
+
+	def update
+		@post=Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			redirect_to posts_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
+
+	end
+
+	def destroy
+		@post=Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"
+
+	end
+
+	def show
+		@post=Post.find(params[:id])
+	end
+
+	def edit
+		@post=Post.find(params[:id])
+	end
+
+	private
+
+    def post_params
+     params.require(:post).permit!
+    end
+
+end
